9f52475293d493b502ffbe7ce4dd6a789e535185
Florence Lee <leeflorence@users.noreply.github.com>
2017-06-14 14:10:31 -0700

    Remove "java.io.InputStream"

--- ContactList.java
+++ ContactList.java
@@ -5,14 +5,13 @@
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Collections;
 
 class ContactList {
 
